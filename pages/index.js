c01bc00236006675b616492a04d8c4b0a3466b1d
jsalvador <jsalvador@grupogarflo.com>
2023-06-14 19:01:05 -0600

    configuraciones iniciales

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <h1 className="font-oxygen text-4xl">Hello Whereas disregard and contempt for human rights have resulted </h1>;
 }
